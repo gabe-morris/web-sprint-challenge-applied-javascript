e25d5db4d205faa12a5712ccf29c46bd7a33ad64
gabe-morris <gabe.e.morris@gmail.com>
2021-07-09 13:17:26 -0500

    completed Header function in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 const Header = (title, date, temp) => {
   //Creates new elements
- const head = document.CreateElement('div')
+ const head = document.createElement('div')
  const headerDate = document.createElement('span')
  const headerTitle = document.createElement('h1')
  const headerTemp = document.createElement('span')
@@ -10,15 +10,22 @@ const Header = (title, date, temp) => {
  headerDate.classList.add('date')
  headerTemp.classList.add('temp')
 
+ //establish hierarchy
+ head.appendChild(headerDate)
+ head.appendChild(headerTitle)
+ head.appendChild(headerTemp)
+
+ //filling out text content
+  headerDate.textContent = date
+  headerTitle.textContent = title
+  headerTemp.textContent = temp
+
+  return head
 }
 
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+  
+
 }
 
 export { Header, headerAppender }
