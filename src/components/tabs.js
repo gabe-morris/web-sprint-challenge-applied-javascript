0566a1163632c73dac7443f8a98f6bf9359e58e0
gabe-morris <gabe.e.morris@gmail.com>
2021-07-09 14:29:49 -0500

    finished tabsAppender function in tabs.js

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const Tabs = (topics) => {
  const topic = document.createElement('div')
   topic.classList.add('topics')
@@ -12,13 +14,13 @@ const Tabs = (topics) => {
 }
 
 const tabsAppender = (selector) => {
-  // TASK 4
-  // ---------------------
-  // Implement this function which takes a css selector as its only argument.
-  // It should obtain topics from this endpoint: `http://localhost:5000/api/topics` (test it in Postman/HTTPie!).
-  // Find the array of topics inside the response, and create the tabs using the Tabs component.
-  // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+ axios.get(`http://localhost:5000/api/topics`)
+ .then (response => {
+   console.log(response)
+   const newTabs = Tabs(response.data.topics)
+   const holder = document.querySelector(selector)
+    return holder.appendChild(newTabs)
+ })
 }
 
 export { Tabs, tabsAppender }
